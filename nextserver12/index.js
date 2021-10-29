af32b1169b5d9ba987a6b4e4df36e46f593d6a0e
Erwin Smit <E.smit@intertrustgroup.com>
2021-10-29 15:51:52 +0200

    initial

---
+++ nextserver12/index.js
@@ -0,0 +1,29 @@
+const { parse } = require('url')
+const next = require("../nextv12/node_modules/next");
+
+const app = next({ 
+    dev: false,
+    conf: {
+        compress: false,
+        distDir: "nextv12/.next",
+    }
+});
+
+const handle = app.getRequestHandler();
+
+module.exports = async function (context, req) {
+    await app.prepare();
+    
+    const parsedUrl = parse("/", true)
+
+    try {    
+        await handle(context.req, context.res, parsedUrl);
+    } catch(e) {
+        console.error(e);
+
+        context.res = {
+            status: 500,
+            body: JSON.stringify(e)
+        };
+    }
+}
